5949b05cf9e95ce65e241f07559d369f4cb3e936
LaelKayo <laelkayo12@gmail.com>
2020-09-10 09:38:37 -0300

    Criando repositorio git

---
+++ src/main/java/br/edu/fafic/ppi/SistemaNossaBibliotecaAvApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fafic.ppi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaNossaBibliotecaAvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaNossaBibliotecaAvApplication.class, args);
+	}
+
+}
